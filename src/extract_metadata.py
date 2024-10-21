1ba55e9a938f78030ef4ff7d6a6f2f52ad9cbfd8
Maryam <mary.elf97@gmail.com>
2024-10-21 13:58:48 -0700

    fully automated metadata extraction

--- src/extract_metadata.py
+++ src/extract_metadata.py
@@ -1,5 +1,7 @@
+import argparse
 from Bio import SeqIO
 import pandas as pd
+from datetime import datetime
 
 # Function to extract geolocation and sample ID from a GenBank record
 def extract_metadata(record):
@@ -23,29 +25,40 @@ def extract_metadata(record):
 
     return strain, date, host, sample_id, geolocation
 
-
-
 # Function to parse a multi-sequence GenBank file and return a DataFrame
 def parse_genbank_file(file_path):
     data = []
-    
     # Parse the GenBank file
     for record in SeqIO.parse(file_path, "genbank"):
         strain, sample_date, host, sample_ID, geolocation = extract_metadata(record)
-        data.append({"strain": strain, "sample_date": sample_date,
-                     "host":host,"accession":sample_ID, "country": geolocation})
-    
+        region = geolocation.split(":")[0].strip() if geolocation else None
+        
+        # Convert the date format from '18-MAY-2021' to '2021-05-18'
+        try:
+            formatted_date = datetime.strptime(sample_date, "%d-%b-%Y").strftime("%Y-%m-%d")
+        except (ValueError, TypeError):
+            formatted_date = None  # Handle cases where date format is not as expected
+
+        data.append({"strain": strain, "accession": sample_ID, "date": formatted_date,
+                     "region": region, "host": host})
     # Convert list of dictionaries to DataFrame
     df = pd.DataFrame(data)
-    
     return df
 
-# Specify the path to your GenBank file
-file_path = "/home/mahmadi/hepE_seqs/sequence.gb"
+def main():
+    # Set up argument parser
+    parser = argparse.ArgumentParser(description='Parse a GenBank file and extract metadata to a TSV file.')
+    parser.add_argument('genbank_file', type=str, help='Path to the input GenBank file')
+    parser.add_argument('output_tsv', type=str, help='Path to the output TSV file')
 
-# Parse the GenBank file and get the DataFrame
-df = parse_genbank_file(file_path)
+    # Parse the arguments
+    args = parser.parse_args()
 
-# Display the DataFrame
-# Optionally, save the DataFrame to a CSV file
-df.to_csv("metadata/metadata.tsv", index=False, sep="\t")
+    # Parse the GenBank file and get the DataFrame
+    df = parse_genbank_file(args.genbank_file)
+
+    # Save the DataFrame to a TSV file
+    df.to_csv(args.output_tsv, index=False, sep="\t")
+
+if __name__ == '__main__':
+    main()
